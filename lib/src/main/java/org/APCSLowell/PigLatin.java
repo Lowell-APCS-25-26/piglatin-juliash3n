865ad9fc85f1094c69ff2621f41f3283c6eb629a
juliash3n <juliashensf@gmail.com>
2026-01-22 06:52:28 +0000

    finished classes

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -4,14 +4,29 @@ public class PigLatin {
     public int findFirstVowel(String sWord) {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    
+        for(int i = 0; i<sWord.length(); i++)
+        if(sWord.substring(i,i+1).equals("a") || sWord.substring(i,i+1).equals("e") || sWord.substring(i,i+1).equals("i") || sWord.substring(i,i+1).equals("o") || sWord.substring(i,i+1).equals("u"))
+        return i;
         return -1;
     }
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-        
+        if(findFirstVowel(sWord)==-1)
+        return sWord+"ay";
+        else if(findFirstVowel(sWord)==0)
+        return sWord + "way";   
+        else if(sWord.substring(0,2).equals("qu"))
+        return sWord.substring(2) + "qu" + "ay";
+        else if(findFirstVowel(sWord)>=1)
+        return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
         return null;
     }
 }
+
+//DONE For words that are all consonants, simply add "ay" to the end of the word. Thus, "try" becomes "tryay".
+//DONE For words that begin with vowels, simply add "way" to the end of the word. Thus, "a" becomes "away"; "at" becomes "atway"; "ermine" becomes "ermineway."
+//DONE For words beginning with "qu," move the "qu" to the end of the word and add ay. Thus "question" becomes "estionquay".
+//DONE For words that begin with consonants, move the leading consonant(s) to the end of the word and add "ay." Thus, "ball" becomes "allbay"; "button" becomes "uttonbay"; "star" becomes "arstay"; "three" becomes "eethray";
+
